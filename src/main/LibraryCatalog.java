df0cb6c5ddd20993a61c6543e2611c935371ef78
yamayrie <124181645+yamayrie@users.noreply.github.com>
2023-10-13 23:12:24 -0400

    deleted unused imports

--- src/main/LibraryCatalog.java
+++ src/main/LibraryCatalog.java
@@ -6,35 +6,13 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.time.LocalDate;
-import java.util.Iterator;
-
-import org.w3c.dom.UserDataHandler;
 
 import data_structures.ArrayList;
 import data_structures.DoublyLinkedList;
 import data_structures.SinglyLinkedList;
 import interfaces.FilterFunction;
 import interfaces.List;
-
-
-/**
- * A DoublyLinkedList was used to store the books and users since we're going to be adding and removing.
- * With a DoublyLinkedList, we can traverse the list forward and backward. This allows us to move efficiently,
- * something that would've given us a bigger time complexity using an ArrayList. If we used an ArrayList,
- * it would've also taken resizing the array whenever we had to insert more books or users. Using an ArrayList
- * would've been an option considered if we didn't have to insert or remove and just had to retrieve data.
- * 
- * Between a SinglyLinkedList and a DoublyLinkedList, I decided to use a DoublyLinkedList. In both we wouldn't
- * have to resize and the insertions and removals are both O(1). DoublyLinkedList just gives us the option of
- * bidirectional traversal, making it the better option. If we had to iterate through the data in different
- * orders, the DoublyLinkedList would make the implementation smoother and easier. It gives us more flexibility
- * in what we do. Even if we will not need to traverse bidirectionally, it is a good option to have.
- * 
-*/
-
-
 
 
 /**
